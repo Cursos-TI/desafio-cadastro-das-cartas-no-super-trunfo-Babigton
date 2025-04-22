a502c5b915756b41cf6d072ed5850b41d9c3a360
Babigton <babigtonfernando@gmail.com>
2025-04-22 14:19:40 +0000

    correcao e pontos e virgulas

--- super_trunfo.c
+++ super_trunfo.c
@@ -10,12 +10,12 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
                                                 // comentarios de dados reais, para facilitar o preenchimento
-    int codigo_cidade, codigo_cidade_2;         //01 / 02
-    char nome_cidade[50], nome_cidade_2[50];    //Juiz de fora //rio de janeiro
-    int populacao, populacao_2;                 //565.764 //6.729,894
-    float area_km, area_km_2;                   //1.435.749 //1.200,329
-    float PIB, PIB_2;                           //35.145,34 //359.630.000,000 bilhoes
-    int pontos_turisticos, pontos_turisticos_2; //29 //58
+    int codigo_cidade, codigo_cidade_2;         // 01            // 02
+    char nome_cidade[50], nome_cidade_2[50];    // Juiz de Fora  // Rio de Janeiro
+    int populacao, populacao_2;                 // 540.756       // 6.211.223
+    float area_km, area_km_2;                   // 1.435,749 km² // 1.200,330 km²    
+    float PIB, PIB_2;                           // 35.145,34     // 53.078,23
+    int pontos_turisticos, pontos_turisticos_2; // 29            // 58                  fonte: IBGE
 
 
     // Cadastro das Cartas:
@@ -32,13 +32,13 @@ int main() {
     printf ("Por favor insira o nome da cidade: ");
     scanf (" %[^\n]", nome_cidade);
 
-    printf ("Por favor insira o numero de habitantes: ");
+    printf ("Por favor insira o numero de habitantes (sem pontos e virgulas): ");
     scanf ("%d", &populacao);
 
-    printf ("Por favor insira  KM²: ");
+    printf ("Por favor insira  KM² (sem pontos e virgulas): ");
     scanf ("%f", &area_km);
 
-    printf ("Por favor insira o PIB: ");
+    printf ("Por favor insira o PIB (sem pontos e virgulas): ");
     scanf ("%f", &PIB);
 
     printf ("Por favor insira numero de pontos turistcos: ");
@@ -54,13 +54,13 @@ int main() {
     printf ("Por favor insira o nome da cidade: ");
     scanf (" %[^\n]", nome_cidade_2);
     
-    printf ("Por favor insira o numero de habitantes: ");
+    printf ("Por favor insira o numero de habitantes (sem pontos e virgulas): ");
     scanf ("%d", &populacao_2);
     
-    printf ("Por favor insira  KM²: ");
+    printf ("Por favor insira  KM² (sem pontos e virgulas): ");
     scanf ("%f", &area_km_2);
     
-    printf ("Por favor insira o PIB: ");
+    printf ("Por favor insira o PIB (sem pontos e virgulas): ");
     scanf ("%f", &PIB_2);
     
     printf ("Por favor insira numero de pontos turistcos: ");
